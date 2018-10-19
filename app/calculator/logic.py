09e849535742ca28444dde21c60a5b844d9dbea3
Sebastian Scharf <sebastian.scharf-ke@freenet.de>
2018-10-19 15:47:19 +0200

    Add raising value error when out of bounds

--- app/calculator/logic.py
+++ app/calculator/logic.py
@@ -15,6 +15,8 @@ class Calculator(object):
         self.max_value = max_value
 
     def mul(self, a, b):
+        if (a<self.min_value or a>self.max_value or b<self.min_value or b>self.max_value):
+            raise ValueError("Out of bounds min/max values uesed. Values should be >= " + str(self.min_value) + " and <= " + str(self.max_value))
         return a*b
 
     def div(self, a, b):
